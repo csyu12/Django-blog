9feb233f0a1cc65c6fc91f26c9641a3894589087
csyu12 <215820356@qq.com>
2020-07-31 04:20:45 +0800

    增加content_html字段，配合保存markdown文本

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,5 @@
+import mistune
+from django.utils.functional import cached_property
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -79,13 +81,14 @@ class Post(models.Model):
     title = models.CharField(max_length=128, verbose_name='标题')
     desc = models.CharField(max_length=1024, blank=True, verbose_name='摘要')
     content = models.TextField(verbose_name='正文', help_text='正文必须为MarkDown格式')
+    content_html = models.TextField(verbose_name='正文html代码', blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.DO_NOTHING)
     tag = models.ManyToManyField(Tag, verbose_name='标签')
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
-    pv = models.PositiveIntegerField(default=1)
-    uv = models.PositiveIntegerField(default=1)
+    pv = models.PositiveIntegerField(default=1)     # 网页点击量，用户对同一页面多次访问，pv累计
+    uv = models.PositiveIntegerField(default=1)     # 浏览网页的自然人，本项目中用uuid指定唯一用户，1天内重复访问同一页面，uv不累计
 
     class Meta:
         verbose_name = verbose_name_plural = '文章'
@@ -93,6 +96,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.content_html = mistune.markdown(self.content)
+        super().save(*args, **kwargs)
 
     @staticmethod   # 声明静态方法，获取标签id指定博客
     def get_by_tag(tag_id):
@@ -122,8 +129,13 @@ class Post(models.Model):
 
     @classmethod    # 获取最新文章
     def latest_posts(cls):
-        return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-uv')
+        return cls.objects.filter(status=cls.STATUS_NORMAL)
 
     @classmethod    # 获取热门文章
     def hot_posts(cls):
         return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
+
+    # 把返回的数据绑到实例上，不用每次访问时都去执行tags函数中的代码
+    @cached_property
+    def tags(self):
+        return ','.join(self.tag.values_list('name', flat=True))
