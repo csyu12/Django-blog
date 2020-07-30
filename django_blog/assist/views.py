28b4938d3127e1ec22a8301478afdda1673ffb29
csyu12 <215820356@qq.com>
2020-07-30 14:27:51 +0800

    优化友链渲染

--- django_blog/assist/views.py
+++ django_blog/assist/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
 
-# Create your views here.
+from blog.views import CommonViewMixin
+from .models import Link
+
+
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'assist/links.html'
+    context_object_name = 'link_list'
+
+
+
